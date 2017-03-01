72145fe2c4015fb9bf32f727ea5b03e2f4dff271
RosaJ <redrose.job@gmail.com>
2017-03-01 21:25:52 +0700

    Update Development Debug File

---
+++ Application/Setting/Debug.php
@@ -0,0 +1,6 @@
+<?php
+define('APP_DEV_CHECK_CSS',true);
+define('APP_DEV_CHECK_JS',true);
+define('APP_NO_CACHE_PLUGIN', true);
+define('APP_NO_CACHE_PHRASE', true);
+define('APP_NO_TEMPLATE', true);
